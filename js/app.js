ae3998f728cae1a5e4ceadeecb1bc892e9dc8ec8
nzeespec <nzeeee1000@gmail.com>
2026-03-01 21:26:57 +0545

    update

---
+++ js/app.js
@@ -0,0 +1,102 @@
+var items = getLocalStorage();
+var editId = null;
+
+function render() {
+  var $app = $("#app");
+  $app.empty();
+
+  var itemToEdit = editId
+    ? $.grep(items, function (item) {
+        return item.id === editId;
+      })[0]
+    : null;
+  var $formElement = createForm(editId, itemToEdit); // edited line
+  var $itemsElement = createItems(items);
+
+  $app.append($formElement);
+  $app.append($itemsElement);
+}
+
+$(document).ready(function () {
+  render();
+});
+
+function getLocalStorage() {
+  var list = localStorage.getItem("grocery-list");
+  if (list) {
+    return JSON.parse(list);
+  }
+  return [];
+}
+
+function setLocalStorage(itemsArray) {
+  localStorage.setItem("grocery-list", JSON.stringify(itemsArray));
+}
+
+function updateItemName(newName) {
+  items = $.map(items, function (item) {
+    if (item.id === editId) {
+      return $.extend({}, item, { name: newName });
+    }
+    return item;
+  });
+  editId = null;
+  setLocalStorage(items);
+  render();
+  setTimeout(function () {
+    alert("Item Updated Successfully!");
+  }, 0);
+}
+
+function setEditId(itemId) {
+  editId = itemId;
+  render();
+
+  setTimeout(function () {
+    $(".form-input").focus();
+  }, 0);
+}
+
+$(document).ready(function () {
+  render();
+});
+
+function editCompleted(itemId) {
+  items = $.map(items, function (item) {
+    if (item.id === itemId) {
+      return $.extend({}, item, { completed: !item.completed });
+    }
+    return item;
+  });
+  setLocalStorage(items);
+  render();
+}
+
+function generateId() {
+  return Date.now().toString(36) + Math.random().toString(36).substr(2);
+}
+
+function addItem(itemName) {
+  var newItem = {
+    name: itemName,
+    completed: false,
+    id: generateId(),
+  };
+  items.push(newItem);
+  setLocalStorage(items);
+  render();
+  setTimeout(function () {
+    alert("Item Added Successfully!");
+  }, 0);
+}
+
+function removeItem(itemId) {
+  items = $.grep(items, function (item) {
+    return item.id !== itemId;
+  });
+  setLocalStorage(items);
+  render();
+  setTimeout(function () {
+    alert("Item Deleted Successfully!");
+  }, 0);
+}
